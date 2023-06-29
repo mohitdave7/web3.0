4a277bf16ca2c041bab0f510c24ec92192dc9a00
mohitdave0@gmail.com <mohitdave0@gmail.com>
2023-06-29 15:45:26 +0530

    added dotenv and deployed contract to sepolia

--- deploy.js
+++ deploy.js
@@ -1,3 +1,4 @@
+require('dotenv').config(); // Load environment variables from .env file
 const HDWalletProvider = require('truffle-hdwallet-provider');
 const Web3  = require('web3');
 const {interface,bytecode} = require('./compile.js');
@@ -10,6 +11,15 @@ const web3 = new Web3(provider);
 const deploy = async()=>{
     const accounts = await web3.eth.getAccounts();
     console.log(accounts,'...');
+    console.log("Contract is deployed by manager with address: " + accounts[0]);
+
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({data:'0x' + bytecode})
+    .send({gas : '2000000', from : accounts[0]})
+    console.log('done'  + result);
+
+    console.log("Contract deployed to address: ", result.options.address)
+    //0x2c987Caa443D52F4322EC6822344b12C38C5cDE6
 }
 
 deploy();
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
- 
+require('dotenv').config(); // Load environment variables from .env file
 
+console.log('process.env.SECRET_KEY',process.env.SECRET_KEY)
+console.log('process.env.SECRET_KEY',process.env.INFURA_URL_OF_SEPOLIA)
 class Lottery extends Component{
     render() {
         return <h1>Decentralized Lottery Application</h1>
